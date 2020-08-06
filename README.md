26d92bcd80b4ed8696e0b5b63134e9ea39b54517
Cameron DeCoster <cameron.decoster@gmail.com>
2020-08-06 07:09:33 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
-# vue-markdown
+# Markdown Previewer
+
+Markdown Previewer allows you to enter markdown in the left pane and shows you a preview of the corresponding HTML in the right pane. It's styled using [Bootstrap](https://getbootstrap.com/) (via [Bootstrap Vue](https://bootstrap-vue.org/)) and compiles the markdown using [Marked](https://github.com/markedjs/marked). This project is based off of [this guide](https://www.freecodecamp.org/news/how-to-build-a-markdown-previewer-with-react-js/) from freeCodeCamp.
+
+You can try the application here: [Markdown Previewer](https://camdecoster.github.io/vue-markdown/).
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <div class="container">
+            <div class="row mt-4">
+                <div class="col text-center">
+                    <h1>
+                        <b-badge class="text-align-center" variant="light">
+                            Markdown Previewer
+                        </b-badge>
+                    </h1>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-md-6">
+                    <div class="col text-center">
+                        <h4>
+                            <b-badge
+                                class="text-align-center"
+                                variant="secondary"
+                            >
+                                Markdown Input
+                            </b-badge>
+                        </h4>
+                    </div>
+                    <div class="mark-input" :style="inputStyle">
+                        <textarea
+                            class="input"
+                            v-model="markdown"
+                            style="padding: 10px"
+                            :style="inputStyle"
+                        >
+                        </textarea>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="col text-center">
+                        <h4>
+                            <b-badge
+                                class="text-align-center"
+                                variant="secondary"
+                            >
+                                Preview
+                            </b-badge>
+                        </h4>
+                    </div>
+                    <div :style="outputStyle" v-html="compiledMarkdown"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Get the marked library for markdown conversion
+import marked from "marked";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    data() {
+        return {
+            inputStyle: {
+                width: "400px",
+                height: "50vh",
+                marginLeft: "auto",
+                marginRight: "auto",
+            },
+            outputStyle: {
+                width: "400px",
+                height: "50vh",
+                backgroundColor: "#DCDCDC",
+                marginLeft: "auto",
+                marginRight: "auto",
+                padding: "10px",
+            },
+            markdown: "# Hello World",
+        };
+    },
+    computed: {
+        // Compile the markdown text to HTML
+        compiledMarkdown() {
+            return marked(this.markdown);
+        },
+    },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
